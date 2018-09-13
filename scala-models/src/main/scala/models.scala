8aa18297b3fa3338d9ae9cd2fd6584e850b0d9e3
Deogle <deogle20@gmail.com>
2018-09-13 18:12:02 -0400

    Added some plots

--- scala-models/src/main/scala/models.scala
+++ scala-models/src/main/scala/models.scala
@@ -16,5 +16,11 @@ object Model1 extends App{
     rg.train().eval()
     println("coefficient = " + rg.coefficient)
     var set = scala.collection.mutable.Set(0,1,2,3)
-    println(rg.backwardElim(set))
+
+    //Forward-Backward elim
+    // println(rg.backwardElim(set))
+
+    //Plotting example
+    Plot(mat.col(0).toInt,mat.col(4).toInt,_title="Title")
+    Plot(mat.col(0).toInt,rg.predict(x).toInt,_title="Title")
 }
